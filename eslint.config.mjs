ba48e5eb7b6ee5413c1fb26bdf5a1c61ab3b6f29
hjsteven <timetobe.kr@gmail.com>
2025-06-23 00:46:03 +0900

    v0.0.1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-console": "warn",
+            "react/no-unescaped-entities": "off",
+            // 필요에 따라 여기에 다른 규칙을 추가하거나 재정의할 수 있습니다.
+        },
+    },
 ];
 
 export default eslintConfig;
